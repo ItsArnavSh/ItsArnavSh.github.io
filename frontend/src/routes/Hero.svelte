10f1588bd5151f242973d88f9ac15bfd2b9c0baa
ItsArnavSh <its.arnav.sh@gmail.com>
2025-02-09 10:57:52 +0530

    Hero Section Done

--- frontend/src/routes/Hero.svelte
+++ frontend/src/routes/Hero.svelte
@@ -12,31 +12,35 @@
 	<!-- Main Content -->
 	<div class="flex w-full flex-col md:flex-row">
 		<!-- Purple Left Side -->
-		<div class="mb-10 ml-40 mt-10 flex w-1/3 rounded-xl bg-[#8B8FE5] md:p-20">
+		<div class="mb-10 ml-10 mt-10 flex w-1/3 rounded-3xl bg-[#8B8FE5] md:p-10">
 			<div class="flex flex-col">
 				<img
 					src="me.png"
 					alt="Profile"
-					class="relative left-32 h-[400px] w-[400px] rounded-full object-cover"
+					class="relative left-0 h-max w-max rounded-full object-cover md:left-32"
 				/>
 				<!-- Left-Aligned "I am" -->
 				<h1 class="text-left text-white">
-					<span class="inter relative right-[30] block text-8xl tracking-wide md:text-9xl"
+					<span class="inter relative right-[30] block text-8xl tracking-wide md:text-8xl"
 						>I am</span
 					>
-					<span class="alkatra md:text-[10]xl relative bottom-5 mt-3 block text-right text-9xl"
+					<span
+						class="alkatra md:text-[15]xl relative bottom-5 left-10 ml-20 mt-3 block text-right text-9xl"
 						>Arnav</span
+					>
+					<span class="alkatra relative bottom-5 left-10 ml-20 mt-3 block text-right md:text-5xl"
+						>.--- ... / .... .- - . .-.</span
 					>
 				</h1>
 			</div>
 		</div>
 
 		<!-- White Right Side -->
-		<div class="flex w-1/2 flex-col items-center justify-center p-8 md:p-12">
+		<div class="flex w-2/3 flex-col items-center justify-center p-8 md:p-12">
 			<!-- Description -->
 			<div class="space-y-6" in:fade={{ duration: 300, delay: 600 }}>
-				<h2 class="anta mb-8 text-5xl font-medium text-gray-800">CSE Undergrad at JIIT.</h2>
-				<p class="anta space-y-4 text-5xl font-bold">
+				<h2 class="fredoka mb-8 text-6xl text-gray-800">CSE Undergrad at JIIT.</h2>
+				<p class="fredoka space-y-8 text-center text-5xl">
 					<span class="inline-block">
 						<span class="relative rounded px-2 py-1">
 							<span class="relative z-10">Backend</span>
@@ -68,7 +72,7 @@
 				</p>
 			</div>
 			<!-- Social Links -->
-			<div class="mb-12 flex flex-row gap-6 p-10">
+			<div class="mb-12 mt-10 flex flex-row gap-6 p-10">
 				{#each socials as { href, icon, label }}
 					<a
 						{href}
